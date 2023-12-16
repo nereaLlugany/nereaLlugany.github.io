d0f7f07542b50ba77455fce2850b9c07ee0525e9
nereaLlugany <149289044+nereaLlugany@users.noreply.github.com>
2023-12-16 04:03:36 +0100

    .

--- main/main_script.js
+++ main/main_script.js
@@ -1,24 +1,9 @@
 function penjarImatge() {
-    let llistaLinks = document.getElementsByTagName("img");
-    let galeria = document.getElementById("imgGaleria");
-    let input = document.getElementById("imatge");
-
-    if (input.files.length > 0) {
-        const file = input.files[0];
-        const reader = new FileReader();
-
-        reader.readAsDataURL(file);
-
-        reader.onload = function () {
-            // Create a new image element
-            const newImage = document.createElement("img");
-
-            // Set the src attribute of the image to the data URL
-            newImage.src = reader.result;
-
-            // Append the new image to the gallery
-            galeria.appendChild(newImage);
-        };
+    let imgInput = document.getElementById("imatge");
+    if (imgInput.length > 0){
+        let fitxer = input.files[0];
+        let formData = new FormData();
+        formData.append('image', file);
     }
 
 }
