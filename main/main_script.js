01320a16d4c6bafa8dba08820a2b3953456d8e0e
nereaLlugany <149289044+nereaLlugany@users.noreply.github.com>
2023-12-23 02:31:20 +0100

    .

--- main/main_script.js
+++ main/main_script.js
@@ -1,13 +1,12 @@
 function penjarImatge() {
-    const input = document.getElementById('imatge');
-    const imagePath = input.value; // Get the file path
+    let input = document.getElementById('imatge');
+    let galeria = document.getElementById('galeria');
+    let imagePath = input.value; 
+    alert(imagePath);
 
     if (imagePath) {
-        const newImage = new Image();
+        let newImage = new Image();
         newImage.src = imagePath; 
-
-        // Add the new image to the gallery
-        const galeria = document.getElementById('galeria');
         galeria.appendChild(newImage);
     } else {
         alert('No file selected');
