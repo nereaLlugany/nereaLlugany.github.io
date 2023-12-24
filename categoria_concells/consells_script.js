c34c3a1e0104dce26ca9b19b871f794ba2a64c50
nereaLlugany <149289044+nereaLlugany@users.noreply.github.com>
2023-12-24 04:56:17 +0100

    .

--- categoria_concells/consells_script.js
+++ categoria_concells/consells_script.js
@@ -29,4 +29,26 @@ window.onload = function(){
     nextBtn.addEventListener('click', () => {
         showSlide(++slideIndex);
     });
+
+    let toggleButton = document.querySelector('.toggle-button');
+    let sidebar = document.querySelector('.sidebar');
+    let closeButton = document.querySelector('.close-button');
+
+    toggleButton.addEventListener('click', function() {
+        if (sidebar.style.transform === 'translateX(-300px)') {
+            sidebar.style.transform = 'translateX(-5px)';
+            setTimeout(function() {
+                closeButton.style.display = 'inline-block';
+            }, 225);
+          } else {
+            sidebar.style.transform = 'translateX(-300px)';
+            closeButton.style.display = 'none';
+          }
+    });
+
+    closeButton.addEventListener('click', function() {
+        sidebar.style.transform = 'translateX(-300px)';
+        closeButton.style.display = 'none';
+    });
+
 }
--- categoria_pastisseria/pastisseria_script.js
+++ categoria_pastisseria/pastisseria_script.js
@@ -25,34 +25,59 @@ function search(listSeeMoreButtons) {
         for (let i = 0; i < recipeSections.length; i++) {
             recipeSections[i].style.display = 'none';
             listSeeMoreButtons[i].style.display = 'none';
+            categoryTitles[i].style.display = 'none'; 
         }
     } else {
         for (let i = 0; i < recipes.length; i++) {
             let recipe = recipes[i];
             let recipeTitle = recipe.textContent.toLowerCase();
+            let categoryTitle = recipe.parentElement.parentElement.parentElement.querySelector('h2');
             let content = recipe.parentElement;
 
             if (recipeTitle.includes(searchQuery)) {
                 content.style.display = 'block';
+                recipeSections[i].style.display = 'block'; 
                 categoryTitle.style.display = 'block';
+                for (let j = 0; j < listSeeMoreButtons.length; j++){
+                    listSeeMoreButtons[j].style.display = 'none';
+                }
+                
             } else {
                 content.style.display = 'none';
                 categoryTitle.style.display = 'none';
-                
-                for (let k = 0; k < listSeeMoreButtons.length; k++) {
-                   listSeeMoreButtons[k].style.display = 'none';
-                }
             }
         }
     }
-
 }
 
+
 window.onload = function() {
+
+    let toggleButton = document.querySelector('.toggle-button');
+    let sidebar = document.querySelector('.sidebar');
+    let closeButton = document.querySelector('.close-button');
+
+    toggleButton.addEventListener('click', function() {
+        if (sidebar.style.transform === 'translateX(-300px)') {
+            sidebar.style.transform = 'translateX(-5px)';
+            setTimeout(function() {
+                closeButton.style.display = 'inline-block';
+            }, 225);
+          } else {
+            sidebar.style.transform = 'translateX(-300px)';
+            closeButton.style.display = 'none';
+          }
+    });
+
+    closeButton.addEventListener('click', function() {
+        sidebar.style.transform = 'translateX(-300px)';
+        closeButton.style.display = 'none';
+    });
+
     let seeMoreButtons = document.querySelectorAll('#seeMoreButton');
     let searchForm = document.querySelector('form');
     let searchInput = searchForm.querySelector('input[name="search"]');
-    let searchButton = searchForm.querySelector('button');
+    let searchButton = searchForm.querySelector('form>button');
 
     for (let i = 0; i < seeMoreButtons.length; i++) {
         let button = seeMoreButtons[i];
