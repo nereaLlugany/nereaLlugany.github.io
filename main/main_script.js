0d74b8e4389dabc05406e588c38c7c8747ba9c7b
nereaLlugany <149289044+nereaLlugany@users.noreply.github.com>
2023-12-28 12:52:40 +0100

    .

--- main/main_script.js
+++ main/main_script.js
@@ -1,6 +1,7 @@
 // Funció per pujar una imatge i afegir-la a una galeria d'imatges
 function penjarImatge(evt) {
     evt.preventDefault(); 
+    console.log(evt.preventDefault());
     // Obtenir el primer arxiu seleccionat a través de l'element amb ID 'imatge'
     let fileInput = document.getElementById('imatge').files[0];
 
